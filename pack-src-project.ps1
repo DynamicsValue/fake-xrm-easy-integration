b5a4cd4e94e2be2da2dc115eab064e503b8dba4e
Jordi <jordi.montana@gmail.com>
2025-01-31 08:27:02 +0100

    Remove FAKE:XRM:EASY config from scripts, update default target framework to net6

--- pack-src-project.ps1
+++ pack-src-project.ps1
@@ -1,6 +1,6 @@
 param (
     [string]$versionSuffix = "",
-    [string]$targetFrameworks = "netcoreapp3.1",
+    [string]$targetFrameworks = "net6.0",
     [string]$packageIdPrefix = "",
     [string]$projectName = "",
     [string]$projectPath = ""   
@@ -18,7 +18,6 @@ Write-Host "Packing All Configurations for project $($projectName)" -ForegroundC
     ./pack-configuration.ps1 -targetFramework $targetFrameworks -projectName $projectName -projectPath $projectPath -packageIdPrefix $packageIdPrefix -versionSuffix $versionSuffix -configuration "FAKE_XRM_EASY_9" 
  }
  else {
-    ./pack-configuration.ps1 -targetFramework $targetFrameworks -projectName $projectName -projectPath $projectPath -packageIdPrefix $packageIdPrefix -versionSuffix $versionSuffix -configuration "FAKE_XRM_EASY" 
     ./pack-configuration.ps1 -targetFramework $targetFrameworks -projectName $projectName -projectPath $projectPath -packageIdPrefix $packageIdPrefix -versionSuffix $versionSuffix -configuration "FAKE_XRM_EASY_2013"
     ./pack-configuration.ps1 -targetFramework $targetFrameworks -projectName $projectName -projectPath $projectPath -packageIdPrefix $packageIdPrefix -versionSuffix $versionSuffix -configuration "FAKE_XRM_EASY_2015"
     ./pack-configuration.ps1 -targetFramework $targetFrameworks -projectName $projectName -projectPath $projectPath -packageIdPrefix $packageIdPrefix -versionSuffix $versionSuffix -configuration "FAKE_XRM_EASY_2016"
